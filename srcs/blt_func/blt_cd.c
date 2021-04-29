c3966c2fa5b1c96c6eb1afb3f22f3aa3e52df311
hyungraelee <hyunlee@student.42seoul.kr>
2021-04-30 00:13:20 +0900

    init blt_cd, error status

--- srcs/blt_func/blt_cd.c
+++ srcs/blt_func/blt_cd.c
@@ -1,32 +1,58 @@
 #include "minishell.h"
 
+void	change_dir(char *dest, char ***envp)
+{
 
-int		exec_cd(t_cmd *cmd_list, char ***envp)
+}
+
+int		exec_cd(t_token *tok, char ***envp)
 {
 	char	dest[PATH_MAX + 1];
 	int		i;
-	t_token	*path;
+	int		j;
+	// t_token	*path;
+	char	*env_value;
 
+	// if (ft_strlen(cmd_list->token->next->arg) > 255)
+		// error
 	i = 0;
 	while (i < PATH_MAX + 1)
 		dest[i++] = 0;
+	while (path->type)
 	path = cmd_list->token->next;
-	if (!path->arg || !ft_strcmp("~", path->arg) || !ft_strcmp("~/", path->arg))
+	i = -1;
+	if (!path || !ft_strcmp("~", path->arg) || !ft_strcmp("~/", path->arg))
 	{
-		find_env_value("HOME", envp);
+		// i = 0;
+		env_value = find_env_value("HOME", *envp);
+		while (*env_value)
+			dest[++i] = (*env_value)++;
 	}
 	else if (path->arg[0] == '~' && path->arg[1] == '/' && ft_strlen(path->arg) > 2)
 	{
-
+		// i = 0;
+		env_value = find_env_value("HOME", *envp);
+		while (*env_value)
+			dest[++i] = (*env_value)++;
+		j = 2;
+		while (path->arg[j])
+			dest[i++] = path->arg[j++];
 	}
 	else if (!ft_strcmp("-", path->arg))
 	{
-
+		// print OLDPWD
+		// i = 0;
+		env_value = find_env_value("OLDPWD", *envp);
+		while (*env_value)
+			dest[++i] = (*env_value)++;
 	}
 	else
 	{
-
+		// i = 0;
+		while (path->arg[++i])
+			dest[i] = path->arg[i];
 	}
+	change_dir(dest, envp);
 }
 
 int		blt_cd(t_cmd *cmd_list, char **envp)
@@ -43,7 +69,7 @@ int		blt_cd(t_cmd *cmd_list, char **envp)
 		{
 			pipe_process(cmd_list);
 			redirect_process(cmd_list, rd_fds);
-			exec_cd(cmd_list, &envp);
+			exec_cd(cmd_list->token, &envp);
 			exit(0);
 		}
 		else if (pid == -1)
@@ -57,6 +83,6 @@ int		blt_cd(t_cmd *cmd_list, char **envp)
 		return (1);
 	}
 	else
-		exec_cd(cmd_list, &envp);
+		exec_cd(cmd_list->token, &envp);
 	return (1);
 }
