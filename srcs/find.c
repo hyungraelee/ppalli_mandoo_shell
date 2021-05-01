e52d1b8e539de4f4572849c6ead1e14d3e34369f
JongeunKeum <jon9eun@gmail.com>
2021-05-01 22:33:53 +0900

    add get_env_name()

--- srcs/find.c
+++ srcs/find.c
@@ -18,7 +18,7 @@ char	*find_env_value(char *env_name, char **envp)
 	return (NULL);
 }
 
-int	find_cmd_path(t_cmd *cmd_list, char **envp)
+int		find_cmd_path(t_cmd *cmd_list, char **envp)
 {
 	char		*env_value;
 	char		**path;
@@ -30,7 +30,7 @@ int	find_cmd_path(t_cmd *cmd_list, char **envp)
 	i = -1;
 	while (path[++i])
 	{
-		path[i] = ft_strjoin(ft_strjoin(path[i], "/"), cmd_list->cmd_name);
+		path[i] = ft_strjoin(ft_strjoin(path[i], "/", 1), cmd_list->cmd_name, 1);
 		if (!stat(path[i], &buf))
 		{
 			cmd_list->cmd_name = ft_strdup(path[i]);
@@ -44,4 +44,77 @@ int	find_cmd_path(t_cmd *cmd_list, char **envp)
 		free(path[i]);
 	free(path);
 	return (run_process(cmd_list, envp));
+}
+
+char	*get_env_value(char *arg, char **env)
+{
+	char	*env_name;
+	char	*result;
+	int		flag;
+	int		i;
+
+	flag = 0;
+	i = 0;
+	result = NULL;
+	while (arg[i])
+	{
+		if (arg[i] == '\'')
+		{
+			if (flag & S_QUOTE)						// if s_quote on
+				flag ^= S_QUOTE;						// s_quote off
+			else if (!(flag & D_QUOTE))				// if d_quote off
+				flag |= S_QUOTE;						// s_quote on
+		}
+		else if (arg[i] == '\"')
+		{
+			if (flag & D_QUOTE)						// if d_quote on
+			{
+				if (arg[i - 1] != '\\')
+					flag ^= D_QUOTE;					// d_quote off
+			}
+			else if (!(flag & S_QUOTE))				// if s_quote off
+				flag |= D_QUOTE;						// d_quote on
+		}
+		else
+		{
+			if (flag & S_QUOTE)
+			{
+				while (flag & S_QUOTE)
+				{
+					result = ft_str_char_join(result, arg[i++]);
+					if (arg[i] == '\'')
+						flag |= S_QUOTE;
+				}
+			}
+			else if (flag & D_QUOTE)
+			{
+				while (flag & D_QUOTE)
+				{
+					if (arg[i] == '\\')
+					{
+						if (arg[i + 1] == '\'' || arg[i + 1] == '\"' || arg[i + 1] == '\\' || arg[i + 1] == '`')
+						{
+							result = ft_str_char_join(result, arg[++i]);
+							i++;
+						}
+						else
+							result = ft_str_char_join(result, arg[i++]);
+					}
+					else if (arg[i] == '$')
+					{
+						if (arg[i - 1] == '\\')
+							result = ft_str_char_join(result, arg[i++]);
+						else
+						{
+							while (!ft_strchr(" \t\n\"\'\\", arg[i]))
+							{
+								// $HOME -> /Users/jkeum
+							}
+						}
+					}
+				}
+			}
+		}
+		i++;
+	}
 }
