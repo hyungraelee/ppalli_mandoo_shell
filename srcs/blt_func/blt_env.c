ca3cd592f6ba15efef1f464ee0bcd8b70cc1eb3e
JongeunKeum <jon9eun@gmail.com>
2021-05-06 16:04:48 +0900

    blt_env.c maybe done

--- srcs/blt_func/blt_env.c
+++ srcs/blt_func/blt_env.c
@@ -1,5 +1,62 @@
 #include "minishell.h"
 
+char	*set_env_value(char *arg, int idx)
+{
+	int		i;
+	int		flag;
+	char	*result;
+
+	i = 0;
+	flag = 0;
+	result = NULL;
+	while (arg[idx + i])
+	{
+		if (arg[idx + i] == '\"' && !(flag & S_QUOTE))
+		{
+			if (flag & D_QUOTE)
+				flag ^= D_QUOTE;
+			else
+				flag |= D_QUOTE;
+			i++;
+		}
+		else if (arg[idx + i] == '\'' && !(flag & D_QUOTE))
+		{
+			if (flag & S_QUOTE)
+				flag ^= S_QUOTE;
+			else
+				flag |= S_QUOTE;
+			i++;
+		}
+		else if (arg[idx + i] == '\\')
+		{
+			if (flag == 0)
+				result = ft_str_char_join(result, arg[idx + ++i]);
+			else if (flag & D_QUOTE)
+			{
+				i++;
+				if (arg[idx + i] != '\"' && arg[idx + i] != '\\' && arg[idx + i] != '`' && arg[idx + i] != '$')
+					result = ft_str_char_join(result, '\\');
+				result = ft_str_char_join(result, arg[idx + i]);
+			}
+			else if (flag & S_QUOTE)
+				result = ft_str_char_join(result, arg[idx + i]);
+			i++;
+		}
+		else
+		{
+			if (arg[idx + i] == ' ' || arg[idx + i] == '\t' || arg[idx + i] == '\n')
+			{
+				result = ft_str_char_join(result, ' ');
+				while (arg[idx + i] == ' ' || arg[idx + i] == '\t' || arg[idx + i] == '\n')
+					i++;
+			}
+			else
+				result = ft_str_char_join(result, arg[idx + i++]);
+		}
+	}
+	return (result);
+}
+
 int	blt_env(t_token *token, char ***envp)
 {
 	int		i;
@@ -17,18 +74,21 @@ int	blt_env(t_token *token, char ***envp)
 			break ;
 	}
 	i = -1;
-	while (*envp[++i])
+	while ((*envp)[++i])
 	{
 		j = 0;
 		env_name = NULL;
-		while (*envp[i][j] && *envp[i][j] != '=')
-			env_name = ft_str_char_join(env_name, *envp[i][j++]);
+		while ((*envp)[i][j] && (*envp)[i][j] != '=')
+			env_name = ft_str_char_join(env_name, (*envp)[i][j++]);
 		if (find_env_value(env_name, *envp))
 		{
 			ft_putstr_fd(env_name, STDOUT_FILENO);
 			ft_putstr_fd("=", STDOUT_FILENO);
-			env_value = NULL;
-			
+			env_value = set_env_value((*envp)[i], ++j);
+			ft_putstr_fd(env_value, STDOUT_FILENO);
+			ft_putstr_fd("\n", STDOUT_FILENO);
+			free(env_value);
 		}
 	}
+	return (1);
 }
