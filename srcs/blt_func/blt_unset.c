9c2634cf7defec90127795b384eb47bb77577d1d
JongeunKeum <jon9eun@gmail.com>
2021-05-07 18:16:21 +0900

    add blt_unset.c (not yet)

---
+++ srcs/blt_func/blt_unset.c
@@ -0,0 +1,50 @@
+#include "minishell.h"
+
+int	blt_unset(t_token *token, char ***envp)
+{
+	int		i;
+	int		is_err;
+	char	*env_name;
+	char	*env_value;
+	char	*new_var;
+
+	while (token)
+	{
+		if (token->type == ARGUMENT)
+		{
+			i = -1;
+			env_name = NULL;
+			while (token->arg[++i])
+			{
+				env_name = ft_str_char_join(env_name, token->arg[i]);
+				if (token->arg[i] == '=')
+				{
+					env_value = set_env_value(set_export_value(token->arg, i, *envp), 0);
+					new_var = ft_strjoin(env_name, env_value, 0);
+					//print	//error (not a valid identifier)
+					return (0);
+				}
+			}
+			if (!token->arg[i])
+			{
+				is_err = set_env_name(&env_name, *envp);
+				if (is_err == 0)
+				{
+
+				}
+				else
+				{
+					if (find_env_name(env_name, *envp) >= 0)
+					{
+						
+					}
+				}
+			}
+		}
+		if (token->next)
+			token = token->next;
+		else
+			break ;
+	}
+	return (1);
+}
