f6464b3ef98ee514915233a71d925363813fe8fb
hyungraelee <timgg12345@gmail.com>
2021-05-11 21:20:52 +0900

    to fix run stat

--- srcs/run/run.c
+++ srcs/run/run.c
@@ -347,6 +347,21 @@ int run(t_cmd *cmd_list, char ***envp)
 			}
 			if (i >= BLTIN_NUM)
 			{
+				// if (stat(cmd_list->cmd_name, &buf) == -1)
+				// {
+				// 	ft_print_err(cmd_list->cmd_name, strerror(errno), NULL, 127);
+				// }
+				// else
+				// {
+				// 	if (S_ISDIR(buf.st_mode))
+				// 		ft_print_err(cmd_list->cmd_name, "is a directory", NULL, 126);
+				// 	else if (S_ISREG(buf.st_mode))
+				// 	{
+				// 		if (!(buf.st_mode & S_IXUSR))
+				// 			ft_print_err(cmd_list->cmd_name, "Permission denied", NULL, 126);
+				// 		else
+				// 	}
+				// }
 				if (!stat(cmd_list->cmd_name, &buf))
 					run_process(cmd_list, envp);
 				else
