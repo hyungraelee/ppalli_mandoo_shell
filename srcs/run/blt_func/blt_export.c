0173ee5a89a6c0877433125d33d627385146fbd4
JongeunKeum <jon9eun@gmail.com>
2021-05-14 20:31:28 +0900

    norminette done

--- srcs/run/blt_func/blt_export.c
+++ srcs/run/blt_func/blt_export.c
@@ -1,43 +1,26 @@
 #include "minishell.h"
 
-void	set_print_export(char *export)
+void	change_export(char *arg, int i, char *name, char ***envp)
 {
-	int		j;
-	char	*export_name;
+	int		chk;
+	char	*value;
+	char	*new_var;
 
-	j = 0;
-	export_name = NULL;
-	while (export[j] && export[j] != '=')
-		export_name = ft_str_char_join(export_name, export[j++]);
-	ft_putstr_fd("declare -x ", STDOUT_FILENO);
-	ft_putstr_fd(export_name, STDOUT_FILENO);
-	print_export_value(export, j, STDOUT_FILENO);
-	ft_putstr_fd("\n", STDOUT_FILENO);
-	if (export_name)
-		free(export_name);
+	new_var = NULL;
+	value = set_export_value(arg, i, *envp);
+	new_var = ft_strjoin(name, value, 0);
+	chk = find_env_name(name, *envp);
+	if (chk >= 0)
+	{
+		ft_strlcpy((*envp)[chk], new_var, ft_strlen(new_var) + 1);
+		free_str(new_var);
+	}
+	else
+		*envp = add_env(*envp, new_var);
+	free_str(value);
 }
 
-void	print_export(char **envp)
-{
-	int		i;
-	char	**export;
-
-	i = 0;
-	while (envp[i])
-		i++;
-	export = (char **)malloc(sizeof(char *) * (i + 1));
-	i = -1;
-	while (envp[++i])
-		export[i] = ft_strdup(envp[i]);
-	export[i] = NULL;
-	sort_export(&export, i);
-	i = -1;
-	while (export[++i])
-		set_print_export(export[i]);
-	free(export);
-}
-
-int		handle_normal_export(char ***envp, char *arg, char *name, char *value)
+int		handle_normal_export(char ***envp, char *arg, char *name)
 {
 	int		i;
 	int		chk;
@@ -48,45 +31,26 @@ int		handle_normal_export(char ***envp, char *arg, char *name, char *value)
 		name = ft_str_char_join(name, arg[i++]);
 	chk = set_env_name(&name, *envp);
 	if (chk == 0)
-  {
-		if (name)
-			free(name);
+	{
+		free_str(name);
 		return (0);
 	}
 	else
-	{
-		new_var = NULL;
-		value = set_export_value(arg, i, *envp);
-		new_var = ft_strjoin(name, value, 0);
-    chk = find_env_name(name, *envp);
-		if (chk >= 0)
-    {
-			ft_strlcpy((*envp)[chk], new_var, ft_strlen(new_var) + 1);
-			if (new_var)
-				free(new_var);
-		}
-		else
-			*envp = add_env(*envp, new_var);
-	}
-	if (name)
-		free(name);
-	if (value)
-		free(value);
+		change_export(arg, i, name, envp);
+	free_str(name);
 	return (1);
 }
 
 int		export_type_arg(char *arg, char ***envp)
 {
 	char	*name;
-	char	*value;
 
 	name = NULL;
-	value = NULL;
 	if (arg[0] == '=')
 		return (0);
 	else
 	{
-		if (!handle_normal_export(envp, arg, name, value))
+		if (!handle_normal_export(envp, arg, name))
 			return (0);
 	}
 	return (1);
