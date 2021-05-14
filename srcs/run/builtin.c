812f66cd62b4f9ddf2152e1ff3b045d474003396
hyungraelee <timgg12345@gmail.com>
2021-05-14 18:43:12 +0900

    norm builtin.c

--- srcs/run/builtin.c
+++ srcs/run/builtin.c
@@ -15,7 +15,7 @@ char	*builtin_str(int i)
 	return (blt_str[i]);
 }
 
-int		(*builtin_func(int i))(t_token *token, char ***envp)
+static int	(*builtin_func(int i))(t_token *token, char ***envp)
 {
 	int		(*blt_func[BLTIN_NUM])(t_token *token, char ***envp);
 
@@ -29,50 +29,45 @@ int		(*builtin_func(int i))(t_token *token, char ***envp)
 	return (blt_func[i]);
 }
 
-void blt_run(int i, t_cmd *cmd_list, char ***envp)
+static void	pipe_blt_run(int i, t_cmd *cmd_list, char ***envp)
 {
-	int status;
+	int	rd_fds[2];
+	int	old_fds[2];
+	int	status;
+
+	save_old_fds(old_fds);
+	pipe(cmd_list->fds);
+	g_global.pid = fork();
+	if (g_global.pid == 0)
+	{
+		pipe_process(cmd_list);
+		if (!redirect_process(cmd_list->token, rd_fds))
+			exit(1);
+		(*builtin_func(i))(cmd_list->token, envp);
+		exit(0);
+	}
+	else if (g_global.pid == -1)
+		ft_print_err("fork", strerror(errno), NULL, 1);
+	else
+	{
+		wait(&status);
+		if (status >> 8 != 0)
+			g_global.exit = status >> 8;
+		redirect_close(rd_fds);
+		pipe_restore(cmd_list, old_fds);
+	}
+}
+
+void		blt_run(int i, t_cmd *cmd_list, char ***envp)
+{
 	int rd_fds[2];
 	int old_fds[2];
 
-	old_fds[0] = dup(STDIN_FILENO);
-	old_fds[1] = dup(STDOUT_FILENO);
+	save_old_fds(old_fds);
 	if (ft_strcmp(cmd_list->cmd_name, "export") && ft_strcmp(cmd_list->cmd_name, "env"))
-	{
-		while (cmd_list->token)
-		{
-			cmd_list->token->arg = get_env_value(cmd_list->token->arg, *envp);
-			if (cmd_list->token->next)
-				cmd_list->token = cmd_list->token->next;
-			else
-				break;
-		}
-		while (cmd_list->token->prev)
-			cmd_list->token = cmd_list->token->prev;
-	}
-	if (cmd_list->prev || cmd_list->next)
-	{
-		pipe(cmd_list->fds);
-		g_global.pid = fork();
-		if (g_global.pid == 0)
-		{
-			pipe_process(cmd_list);
-			if (!redirect_process(cmd_list->token, rd_fds))
-				exit(1);
-			(*builtin_func(i))(cmd_list->token, envp);
-			exit(0);
-		}
-		else if (g_global.pid == -1)
-			ft_print_err("fork", strerror(errno), NULL, 1);
-		else
-		{
-			wait(&status);
-			if (status >> 8 != 0)
-				g_global.exit = status >> 8;
-			redirect_close(rd_fds);
-			pipe_restore(cmd_list, old_fds);
-		}
-	}
+		replace_env_in_arg(cmd_list, envp);
+	if (is_pipe_exist(cmd_list))
+		pipe_blt_run(i, cmd_list, envp);
 	else
 	{
 		if (!redirect_process(cmd_list->token, rd_fds))
